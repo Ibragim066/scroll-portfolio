10bb0baf8dcec5ae7950a93f6103e823f3543d4d
Ibragim <gamemega.ao@gmail.com>
2023-07-15 11:39:31 +0300

    optimizated js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-slider1 = new Swiper('.slider1', {
-	freeMode: true,
-	centeredSlides: true,
-	direction: 'vertical',
-	mousewheel: true,
-	slidesPerView: 1.75,
+document.querySelectorAll('.slider').forEach((n, i) => {
+	window[`slider${i + 1}`] = new Swiper(n, {
+		freeMode: true,
+		centeredSlides: true,
+		direction: 'vertical',
+		mousewheel: true,
+		slidesPerView: 1.75,
+		slidesOffsetBefore: -125,
+	})
 })
